d9d9242e5f394b7e51c54a75a096203b542de1bf
Rafael <rafaeldealmeida.192001@gmaill.com>
2022-12-20 20:15:02 -0300

    Separando arquivos por ambiente e funcao e declarando variaveis.

---
+++ infra/variables.tf
@@ -0,0 +1,13 @@
+#Declaracao de variables
+#Valores serao passados dentro dos ambientes
+variable "regiao_aws" {
+    type = string
+}
+
+variable "chave" {
+    type = string
+}
+
+variable "instancia"{
+    type = string
+}
