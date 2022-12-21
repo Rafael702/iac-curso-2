a621c3cb0b5a68dcd08cba3a6dde3c3e9630f23d
Rafael <rafaeldealmeida.192001@gmaill.com>
2022-12-21 08:57:35 -0300

    *Desafio - Atribuindo valores as variaveis em prod

---
+++ env/Prod/main.tf
@@ -0,0 +1,8 @@
+#As variaveis declaradas na infra receberao o seu valor aqui.
+module "aws-prod" {
+  source = "../../infra"
+  ami = "ami-0b0d54b52c62864d6"
+  instancia  = "t2.micro"
+  regiao_aws = "sa-east-1"
+  chave = "Iac-prod"
+}
