6754b54c8c3c444d412d4fc9d759620ff9a8be9f
Rafael <rafaeldealmeida.192001@gmaill.com>
2022-12-21 08:56:34 -0300

    Atribuindo valores as variaveis em dev

---
+++ env/Dev/main.tf
@@ -0,0 +1,8 @@
+#As variaveis declaradas na infra receberao o seu valor aqui.
+module "aws-dev" {
+  source = "../../infra"
+  ami = "ami-0ecc74eca1d66d8a6"
+  instancia  = "t2.micro"
+  regiao_aws = "us-west-2"
+  chave = "Iac-Dev"
+}
